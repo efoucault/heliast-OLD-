3874b881e3d4051ded5030db806708224737ff1e
Ahmed Menouni <ahmed.menouni@gmail.com>
2018-08-26 21:34:44 +0200

    active admin basic setup

---
+++ app/admin/historiques.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Historique do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :user_id, :indecision_id, :date_relance, :status, :date_fin, :created_at, :updated_at
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
---
+++ app/admin/users.rb
@@ -0,0 +1,25 @@
+ActiveAdmin.register User do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :email, :encrypted_password, :reset_password_token, :remember_created_at, :created_at, :updated_at, :first_name, :last_name, :level, :admin
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  index do
+    selectable_column
+    column :id
+    column :email
+    column :name
+    column :created_at
+    column :admin
+    actions
+  end
+
+end
